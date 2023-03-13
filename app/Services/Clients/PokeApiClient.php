c06b907f9cde7fcba2dfb8ad23d8ff6cc6b62497
Leonardo Castro de Oliveira <leonardo.castro@ambrait.com>
2023-03-13 01:21:16 -0300

    feat: PokeAPIClient constructor and method to get Ability

--- app/Services/Clients/PokeApiClient.php
+++ app/Services/Clients/PokeApiClient.php
@@ -2,14 +2,49 @@
 
 namespace App\Services\Clients;
 
+use App\Exceptions\MissingEnvValueException;
+use App\Exceptions\PokeApiClienItemNotFoundException;
+use Exception;
 use Illuminate\Support\Facades\Http;
 
 
 class PokeApiClient
 {
-    public function getPokemonById(int|string $id) {
-        return json_decode(
-                Http::get("https://pokeapi.co/api/v2/pokemon/{$id}/"
+    private string $baseUri;
+
+    public function __construct()
+    {
+        $this->baseUri = env('POKE_API_CLIENT_URL');
+
+        if (empty($this->baseUri)) {
+            throw MissingEnvValueException::for('POKE_API_CLIENT_URL');
+        }
+    }
+
+    public function getPokemonById(int|string $id)
+    {
+        $pokemon = json_decode(
+                Http::get($this->baseUri . "/pokemon/{$id}/"
             )->getBody()->getContents());
+
+        if (is_null($pokemon)) {
+            throw new PokeApiClienItemNotFoundException($id);
+        }
+
+        return $pokemon;
+    }
+
+    public function getAbilityById(int|string $id)
+    {
+        $ability = json_decode(
+            Http::get(
+                $this->baseUri . "/ability/{$id}/"
+            )->getBody()->getContents()
+        );
+        
+        if (is_null($ability)) {
+            throw new PokeApiClienItemNotFoundException($id);
+        }
+        return $ability;
     }
 }
