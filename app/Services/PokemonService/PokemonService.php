e853d7e2994c54828521314bc54568d124e40163
Leonardo Castro de Oliveira <leonardo.castro@ambrait.com>
2023-03-13 01:24:19 -0300

    feat: ability seeder

--- app/Services/PokemonService/PokemonService.php
+++ app/Services/PokemonService/PokemonService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services\PokemonService;
 
+use App\Exceptions\PokeApiClienItemNotFoundException;
+use App\Models\Ability;
 use App\Models\Pokemon;
 use App\Services\Clients\PokeApiClient;
 
@@ -9,8 +11,7 @@ class PokemonService
 {
     public function __construct(
         private PokeApiClient $pokeApiClient,
-    ) {
-    }
+        ) {}
 
     public function externalFetchAndSave()
     {
@@ -22,11 +23,41 @@ class PokemonService
                     'name' => $pokemon->name,
                     'height' => $pokemon->height,
                     'weight' => $pokemon->weight,
+                    'base_experience' => $pokemon->weight,
                     'sprites' => json_encode($pokemon->sprites)                
                 ]
             );
         }
         return $pokemon;
+    }
+
+    public function abilityExternalFetchAndSave()
+    {
+        try {
+            for ($id = 1; $id <= 300; $id++) {
+                    $ability = $this->pokeApiClient->getAbilityById($id);
+                    $englishDescription = "";
+
+                    foreach ($ability->effect_entries as $description)
+                        {
+                            if ($description->language->name == "en") {
+                                $englishDescription = $description->short_effect;
+                                break;
+                            }
+                        }
+                    Ability::updateOrCreate(
+                        ['id' => $id],
+                        [
+                            'name' => $ability->name,
+                            'description' => $englishDescription,
+                        ]
+                    );
+            } 
+        } catch (PokeApiClienItemNotFoundException $e) {
+            logger($e->message($id));
+            $lastInsert = $id - 1;
+            logger("Ability - Last inserted ID was $lastInsert");
+        }
     }
 
     public function fetchById(int $id)
