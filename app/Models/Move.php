11acb932049eb6ff0f8b802f0222f2ffd2294b9f
Leonardo Castro de Oliveira <leonardo.oliveira951@gmail.com>
2023-03-20 23:23:00 -0300

    feat: added fillable array into root models

--- app/Models/Move.php
+++ app/Models/Move.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Move extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+    ];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
