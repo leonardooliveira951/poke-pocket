cfb6914e7a71bbed8caeb5452f040de348d63ccf
Leonardo Castro de Oliveira <leonardo.oliveira951@gmail.com>
2023-03-28 23:01:42 -0300

    refactor: abstracted pokeApi GET method

--- app/Services/Clients/PokeApiClient.php
+++ app/Services/Clients/PokeApiClient.php
@@ -21,86 +21,19 @@ class PokeApiClient
         }
     }
 
-    public function getPokemonById(int|string $id)
+    public function abstractGet(string $entityName, int $id)
     {
-        $pokemon = json_decode(
-                Http::get($this->baseUri . "/pokemon/{$id}/"
-            )->getBody()->getContents());
-
-        if (is_null($pokemon)) {
-            throw new PokeApiClienItemNotFoundException($id);
-        }
-
-        return $pokemon;
-    }
-
-    public function getAbilityById(int|string $id)
-    {
-        $ability = json_decode(
+        $item = json_decode(
             Http::get(
-                $this->baseUri . "/ability/{$id}/"
-            )->getBody()->getContents()
-        );
-        
-        if (is_null($ability)) {
-            throw new PokeApiClienItemNotFoundException($id);
-        }
-        return $ability;
-    }
-
-    public function getTypeById(int|string $id)
-    {
-        $ability = json_decode(
-            Http::get(
-                $this->baseUri . "/type/{$id}/"
+                $this->baseUri . "/{$entityName}/{$id}/"
             )->getBody()->getContents()
         );
 
-        if (is_null($ability)) {
-            throw new PokeApiClienItemNotFoundException($id);
+        if (is_null($item)) {
+            throw new PokeApiClienItemNotFoundException($id, $entityName);
         }
-        return $ability;
+
+        return $item;
     }
 
-    public function getMoveDmgClassById(int|string $id)
-    {
-        $moveDmgClass = json_decode(
-            Http::get(
-                $this->baseUri . "/move-damage-class/{$id}/"
-            )->getBody()->getContents()
-        );
-
-        if (is_null($moveDmgClass)) {
-            throw new PokeApiClienItemNotFoundException($id);
-        }
-        return $moveDmgClass;
-    }
-
-    public function getMoveById(int|string $id)
-    {
-        $move = json_decode(
-            Http::get(
-                $this->baseUri . "/move/{$id}/"
-            )->getBody()->getContents()
-        );
-
-        if (is_null($move)) {
-            throw new PokeApiClienItemNotFoundException($id);
-        }
-        return $move;
-    }
-
-    public function getStatById(int|string $id)
-    {
-        $stat = json_decode(
-            Http::get(
-                $this->baseUri . "/stat/{$id}/"
-            )->getBody()->getContents()
-        );
-
-        if (is_null($stat)) {
-            throw new PokeApiClienItemNotFoundException($id);
-        }
-        return $stat;
-    }
 }
